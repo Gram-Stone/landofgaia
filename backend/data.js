67b7a7279871d4a818a6373db35993714fb2ef9f
Gram Stone <stonegram@gmail.com>
2021-08-15 22:04:17 -0400

    Start backend; install express,  nodemon, eslint

---
+++ backend/data.js
@@ -0,0 +1,48 @@
+const data = {
+  products: [
+    {
+      _id: "1",
+      name: "Boho Summer Dress",
+      category: "Dresses",
+      image: "/images/p1.jpg",
+      price: 45.0,
+    },
+    {
+      _id: "2",
+      name: "Boho Summer Dress",
+      category: "Dresses",
+      image: "/images/p1.jpg",
+      price: 45.0,
+    },
+    {
+      _id: "3",
+      name: "Boho Summer Dress",
+      category: "Dresses",
+      image: "/images/p1.jpg",
+      price: 45.0,
+    },
+    {
+      _id: "4",
+      name: "Boho Summer Dress",
+      category: "Dresses",
+      image: "/images/p1.jpg",
+      price: 45.0,
+    },
+    {
+      _id: "5",
+      name: "Boho Summer Dress",
+      category: "Dresses",
+      image: "/images/p1.jpg",
+      price: 45.0,
+    },
+    {
+      _id: "6",
+      name: "Boho Summer Dress",
+      category: "Dresses",
+      image: "/images/p1.jpg",
+      price: 45.0,
+    },
+  ],
+};
+
+export default data;
