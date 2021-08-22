ddbb40e92fcb067be0934b0c08e2a5f2b16f8293
Gram Stone <stonegram@gmail.com>
2021-08-21 23:58:49 -0400

    Added redux to home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
 import Product from "../components/Product";
 import MessageBox from "../components/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 export default function HomeScreen() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("/api/products");
-        setLoading(false);
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <div>
-      {error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
+      {loading ? (
+        <div></div>
+      ) : error ? (
+        <MessageBox>{error}</MessageBox>
       ) : (
-        <div>
+        <div className="row center">
           {products.map((product) => (
             <Product key={product._id} product={product}></Product>
           ))}
